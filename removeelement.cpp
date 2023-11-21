cf5d3642b8b4543e7b27d4408ced13a38dd6737c
raghavkapoor-prog <114831248+raghavkapoor-prog@users.noreply.github.com>
2023-11-21 13:08:28 +0530

    Create removeelement.cpp
    remove element

---
+++ removeelement.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    int n ;
+    int nums[n];
+    int val;
+    int j =0;
+    for (int i =0; i<n; i++){
+        if (nums[i]!=val){
+            nums[j]= nums [i];
+        }
+
+    }
+}
