661f8b5e66da2ac1d74b4070ed9d116c8d279c9b
raghavkapoor-prog <114831248+raghavkapoor-prog@users.noreply.github.com>
2023-11-23 20:14:22 +0530

    jump game.cpp

---
+++ jumpgame.cpp
@@ -0,0 +1,31 @@
+
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        return solve(0,nums);
+    }
+    
+    bool solve(int i,vector<int> nums){
+        
+        if(i==nums.size()-1) return true;
+        int reachable=i+nums[i];
+        
+        for(int k=i+1;k<=reachable;k++){
+                  if(solve(k,nums)){
+                return true;
+            }      
+        }
+        return false;
+    }
+   
+};
+
+int n=nums.size();
+        int reachable=0;
+        
+        for(int i=0;i<n;i++){
+            if(i>reachable) return false;
+            reachable=max(reachable,i+nums[i]);
+        }
+        return true;
